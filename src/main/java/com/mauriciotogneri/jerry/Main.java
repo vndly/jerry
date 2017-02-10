af48d00d59bc279c4ef4f36b7e436e32078766ab
Mauricio Togneri <mauricio.togneri@gmail.com>
2017-02-10 20:34:10 +0100

    Changed port

--- src/main/java/com/mauriciotogneri/jerry/Main.java
+++ src/main/java/com/mauriciotogneri/jerry/Main.java
@@ -19,7 +19,7 @@ public class Main
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
 
-        Server jettyServer = new Server(8080);
+        Server jettyServer = new Server(80);
         jettyServer.setHandler(context);
 
         ServletHolder jerseyServlet = context.addServlet(ServletContainer.class, "/*");
